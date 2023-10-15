f9f188b2fbbfd24de09352967eceb7a837f784f7
Frank Kim <fkim98@hotmail.com>
2023-10-14 20:21:09 -0700

    Improve the loading experience

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,4 +1,10 @@
-import IssueForm from "../_components/issueForm";
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 const NewIssuePage = () => {
   return <IssueForm />;
