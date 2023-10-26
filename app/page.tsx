78ef4332242e6efa365e297c6d99eff59f60245f
Frank Kim <fkim98@hotmail.com>
2023-10-26 12:36:52 -0700

    Modify the title

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,6 @@ export default async function Home() {
 export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
-  title: "Issue Tracker - Dashboard",
+  title: "Issue Tracker R - Dashboard",
   description: "View a summary of project issues",
 };
